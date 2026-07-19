33a418769a45fd391b0dec3ef2ca77808e6909af
Amal Raj M P <amalrajmp72@gmail.com>
2026-07-19 20:38:48 +0530

    Make ChromaDB URL configurable

--- server/services/rag/chroma.js
+++ server/services/rag/chroma.js
@@ -1,5 +1,7 @@
 import { ChromaClient } from "chromadb"
 
-const client = new ChromaClient()
+const client = new ChromaClient({
+  path: process.env.CHROMA_URL || "http://localhost:8000",
+})
 
 export default client
